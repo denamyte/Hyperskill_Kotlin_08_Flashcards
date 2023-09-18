9f88c63ecaf7521dde431dcf0ad37ad49d2c2bc2
denys <denamyte@gmail.com>
2023-09-18 23:28:19 +0300

    2023-09-18: Stage 4/7

--- Flashcards (Kotlin)/task/src/flashcards/Cards.kt
+++ Flashcards (Kotlin)/task/src/flashcards/Cards.kt
@@ -2,18 +2,20 @@ package flashcards
 
 class Cards {
     private val map = mutableMapOf<String, String>()
+    private val oppMap = mutableMapOf<String, String>()
     val cards get() = map.toMap()
 
     fun add(term: String, definition: String) {
         map[term] = definition
+        oppMap[definition] = term
     }
 
-//    fun check(term: String, definition: String) = map[term] == definition
+    fun containsTerm(term: String) = term in map.keys
 
-//    fun str(term: String) = """
-//        Card:
-//        $term
-//        Definition:
-//        ${map[term]}
-//    """.trimIndent()
+    fun containsDefinition(definition: String) = definition in map.values
+
+    fun definition(term: String) = map[term]!!
+
+    fun term(definition: String) = oppMap[definition]!!
+
 }
--- Flashcards (Kotlin)/task/src/flashcards/Runtime.kt
+++ Flashcards (Kotlin)/task/src/flashcards/Runtime.kt
@@ -1,7 +1,7 @@
 package flashcards
 
 class Runtime {
-    val cards = Cards()
+    private val cards = Cards()
 
     fun run() {
         val n = inputN()
@@ -16,17 +16,41 @@ class Runtime {
 
     private fun inputCards(n: Int) {
         for (i in 1..n) {
-            println("Card #$i:")
-            val term = readln()
-            println("The definition for card #$i:")
-            cards.add(term, readln())
+            val term = inputCardPart(
+                "Card #$i:",
+                cards::containsTerm,
+                "The term \"%s\" already exists. Try again:"
+            )
+            val def = inputCardPart(
+                "The definition for card #$i:",
+                cards::containsDefinition,
+                "The definition \"%s\" already exists. Try again:"
+            )
+
+            cards.add(term, def)
+        }
+    }
+
+    private fun inputCardPart(prompt: String,
+                              exists: (String) -> Boolean,
+                              error: String): String {
+        println(prompt)
+        while (true) {
+            val value = readln()
+            if (exists(value))
+                println(error.format(value))
+            else return value
         }
     }
 
     private fun checkCards() = cards.cards.forEach { (t, d) ->
         println("Print the definition of \"$t\":")
-        (if (readln() == d) "Correct!"
-        else "Wrong. The right answer is \"$d\".")
-            .run(::println)
+        val userDef = readln()
+        println(when {
+            userDef == d -> "Correct!"
+            cards.containsDefinition(userDef) -> "Wrong. The right answer is \"$d\", " +
+                    "but your definition is correct for \"${cards.term(userDef)}\"."
+            else -> "Wrong. The right answer is \"$d\"."
+        })
     }
 }
